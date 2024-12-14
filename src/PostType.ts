2301ec6117bb788897708731d016ab3bf355b454
Umuthan Özel <ozelumuthan@gmail.com>
2024-12-14 21:12:07 +0300

    Rename thumbnail_string to thumbnail_url in PostType definition

--- src/PostType.ts
+++ src/PostType.ts
@@ -5,7 +5,7 @@ export type PostType = {
   content: string;
   url_string: string;
   views: number;
-  thumbnail_string: string;
+  thumbnail_url: string;
   is_hidden: boolean;
   is_draft: boolean;
   is_pinned: boolean;
