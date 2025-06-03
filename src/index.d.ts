21bec447c4ec736d4abdef95fdc73b0f9a28d8db
Umuthan Özel <ozelumuthan@gmail.com>
2025-06-03 12:33:03 +0300

    Export GameState

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,4 @@ export { PostType } from "./PostType";
 export { FeaturedType } from "./FeaturedType";
 export { GameLogType } from "./GameLogType";
 export { GameTagType } from "./GameTagType";
+export { GameState } from "./GameLogType";
